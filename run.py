d94fe15526a7a46c513358e45b623172f1cc0a7c
TheBlep <soshire@protonmail.com>
2025-02-06 13:44:32 +0100

    build(logic): Placed initial functions for player turn and game setup

--- run.py
+++ run.py
@@ -17,3 +17,28 @@ def place_ships(board, num_ships):
             board[x][y] = 'S'
             ships += 1
 
+def get_user_guess():
+    while True:
+        guess = input("Enter your guess (row and column, e.g., 2 3): ").split()
+        if len(guess) == 2 and guess[0].isdigit() and guess[1].isdigit():
+            return int(guess[0]), int(guess[1])
+        else:
+            print("Invalid input. Please enter two numbers separated by a space.")
+
+
+def setup_game(board_size=5, num_ships=3):
+    player1_board = create_board(board_size)
+    player2_board = create_board(board_size)
+    
+    print("Player 1, place your ships:")
+    place_ships(player1_board, num_ships)
+    print_board(player1_board)
+    
+    print("Player 2, place your ships:")
+    place_ships(player2_board, num_ships)
+    print_board(player2_board)
+    
+    return player1_board, player2_board
+
+
+
