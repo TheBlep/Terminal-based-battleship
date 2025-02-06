483ec3b26dd33659b1b03ba9b4340b5e36c10b9c
TheBlep <soshire@protonmail.com>
2025-02-06 10:28:22 +0100

    build(logic): added random ship placement

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Its a battleship game! woo!
+
+# Setting up grid constants
+GRID_SIZE = 10
+SHIPS = {'Destroyer': 2, 'Submarine': 3, 'Battleship': 4}
+
+# Playing feilds
+player_grid = [['~'] * GRID_SIZE for _ in range(GRID_SIZE)]
+enemy_grid = [['~'] * GRID_SIZE for _ in range(GRID_SIZE)]
+
+# Placing ships randomly
+import random
+def random_row():
+    return random.randint(0,GRID_SIZE -1)
+
+def random_col():
+    return random.randint(0,GRID_SIZE -1)
+
+#def place_ships(ships, size, grid)
