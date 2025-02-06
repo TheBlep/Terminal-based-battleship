4581313bafc037ee1d51526053ac65cbc5eedc99
TheBlep <soshire@protonmail.com>
2025-02-06 13:43:33 +0100

    build(logic): Placed initial functions for board and ships

--- run.py
+++ run.py
@@ -1,3 +1,19 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+
+def create_board(size):
+    return [['~'] * size for _ in range(size)]
+
+def print_board(board):
+    for row in board:
+        print(' '.join(row))
+    print()
+
+def place_ships(board, num_ships):
+    ships = 0
+    while ships < num_ships:
+        x, y = random.randint(0, len(board) - 1), random.randint(0, len(board) - 1)
+        if board[x][y] == '~':
+            board[x][y] = 'S'
+            ships += 1
+
